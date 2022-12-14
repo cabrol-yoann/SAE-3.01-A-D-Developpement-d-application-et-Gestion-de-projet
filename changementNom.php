62d19b66a4e4d864223934264c7d32fd65399fe4
yoann cabrol <cabrolyoann@hotmail.fr>
2022-12-14 11:47:36 +0100

    Correction complet de la fonctionnalité changementNom

--- changementNom.php
+++ changementNom.php
@@ -1,51 +1,50 @@
 <?php
 
-function ChangementNomDossier($nomDossierTrouver,$ObjetAPlacer){
+include_once "baseDeDonnéePhysique.php";
+include_once "Fichier.php";
+include_once "Dossier.php";
 
-    include_once "Fichier.php";
-    include_once "Dossier.php";
-    include_once "baseDeDonnéePhysique.php";
+function ChangementNomDossier($nomDossierTrouver, $ObjetAPlacer){
 
-    //Variables
-    $listeEnfantDossier = $nomDossierTrouver->getListeEnfantDossier();
-    $conteur=1;
-    $nouveauNom = "";
-    $testDossier = new Dossier("",0,"");
-    print_r($objetAPlacer);
+    if (get_class($ObjetAPlacer) == get_class($testDossier = new Dossier("",0,""))) {
+        //Variables
+        $compteur=1;
+        $nouveauNom = "";
+        $listeEnfantDossier = $nomDossierTrouver->getListeEnfantDossier();
 
-    if (get_class($objetAPlacer) == get_class($testDossier)) {
         //Pour changer le nom d'un dossier
         while ($listeEnfantDossier->valid()) {
             $DossierTraiter= $listeEnfantDossier->current();
             if ($DossierTraiter->getNom() == $ObjetAPlacer->getNom()) {
-                $nouveauNom = $objetAPlacer->getNom()."(".$conteur.")";
+                $nouveauNom = $ObjetAPlacer->getNom()."(".$compteur.")";
                 $ObjetAPlacer->setNom($nouveauNom);
-                $conteur++;
-                echo 'entre <br>';
+                $compteur++;
+                $listeEnfantDossier->rewind();
             }
             $listeEnfantDossier->next();
         }
     }
-    if(get_class($objetAPlacer) == get_class($testFichier = new Fichier("",0,"",""))) {
+    if(get_class($ObjetAPlacer) == get_class($testFichier = new Fichier("",0,"",""))) {
         //Variables
         $listeEnfantFichier= $nomDossierTrouver->getListeEnfantFichier();
-        $conteur=1;
+        $compteur=1;
         $nouveauNom = "";
     
-        //Pour changer le nom d'un dfichier
-        while (TRUE) {
-            if ($nomDossierTrouver->listeEnfantFichier[$conteur]->getNom() == $ObjetAPlacer->getNom()) {
-                $nouveauNom = $objetAPlacer->getNom()."(".$conteur.")";
+        //Pour changer le nom d'un fichier
+        while ($listeEnfantFichier->valid()) {
+            $FichierTraiter = $listeEnfantFichier->current();
+            if ($FichierTraiter->getNom() == $ObjetAPlacer->getNom()) {
+                $nouveauNom = $ObjetAPlacer->getNom()."(".$compteur.")";
                 $ObjetAPlacer->setNom($nouveauNom);
-                $conteur++;
+                $compteur++;
+                $listeEnfantFichier->rewind();
+                print($nouveauNom);
             }
             $listeEnfantFichier->next();
         }
     }
 }
 
-
-include_once "baseDeDonnéePhysique.php";
 ChangementNomDossier($dossier4,$objetAPlacer);
 echo 'fin';
 
