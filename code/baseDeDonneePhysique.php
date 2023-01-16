a845462c29e0db80ac23b24ceae6f9a88577b370
yoann cabrol <cabrolyoann@hotmail.fr>
2023-01-16 12:09:13 +0100

    ajouter d'une page qui affiche les espacesStockage

--- code/baseDeDonneePhysique.php
+++ code/baseDeDonneePhysique.php
@@ -8,6 +8,10 @@ include_once "../classe/Fichier.php";
 $drive = new Stockage("SAE",5,"/",500000000,false);
 $cloud = new Stockage("SAE",5,"/",666690420,true);
 $FTP = new Stockage("Stockage",5,"/",987654321,true);
+$Stockage = new \SplObjectStorage();
+$Stockage->attach($drive);
+$Stockage->attach($cloud);
+$Stockage->attach($FTP);
 
 //DOSSIER
 $dossier1 = new Dossier("TD",1120970,"/TD");
--- web/index.php
+++ web/index.php
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Duolcloud</title>
-    <link rel="stylesheet" href="">
-</head>
-<body>
-    <!-- page html avec header qui possede un nav avec 2 colonnes "doc","programme". un body avec 3 tableaux qui comprend 4 lignes. Dans un div un formulaire qui rentre 3 élément "nom","taille","chemin" -->
+<?php
+
+include_once "../code/baseDeDonneePhysique.php";
+
+echo '<!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Duolcloud</title>
+        <link rel="stylesheet" href="">
+    </head>
+    <body>';
+    while($Stockage->valid()) {
+        echo '<h1>'.$Stockage->current()->getNom().'</h1>';
+        echo '<p>'.$Stockage->current()->getNom().'NOM <br>'
+        .$Stockage->current()->getTaille().'Taille <br>'
+        .$Stockage->current()->getTailleMax().'TAILLEMAX <br>'
+        .$Stockage->current()->getChemin().'CHEMIN <br>'
+        .$Stockage->current()->getRestructurable().'RESTRUCTURATION <br>
+        </p>';
+        $Stockage->next();
+    }
+echo '</body>
+    </html>';
     
-    rehtehre
-</body>
-</html>
+?>
