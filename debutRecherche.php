bf446eb2125d5927a0ccdcb4b2066b4a909932d3
RomainG <rgouaud@iutbayonne.univ-pau.fr>
2023-01-16 11:15:50 +0100

    tentative de correction de debutRecherche

--- debutRecherche.php
+++ debutRecherche.php
@@ -1,11 +1,15 @@
 <?php
 
+include "baseDeDonnéePhysique.php";
+
+
+
 function debutRecherche ($stockage, $objetAPlacer){
 //Meilleur Emplacement
     //initialisation
     $score = 0;
     $nomDossierTrouver = "";
-    $listStockage = array("","","","");
+    $listStockage = new \SplObjectStorage(); 
     $trouver = false;
     $tailleCalculer = 0;
 
@@ -14,13 +18,14 @@ function debutRecherche ($stockage, $objetAPlacer){
         //Calcul de la taille de l'espace de stockage avec le dossier à placer
         $tailleCalculer = $nomStockage->getTaille() + $objetAPlacer->getTaille();
 
-        //On regarde si on peut stocker le dossier dans un espace puis enregistre la valeur dans une liste
-        if ($tailleCalculer > $nomStockage->getTailleMax()) {
-            $restructurationEnCour = false;
-            if ($nomStockage->getRestructurable() == true) {
-                $nomStockage = $stockageTravailler[$iterateur];
+        //On regarde si on peut stocker le dossier dans un espace puis on enregistre la valeur dans une liste
+        if ($tailleCalculer > $nomStockage->getTailleMax()) { // Non restructurable
+            if ($nomStockage->getRestructurable() == true) {  // Mais taille restante suffisante
+                $nomStockage = $stockageTravailler[$iterateur]; 
+                $listStockage->insertValues($nomStockage); 
             }
-        } elseif ($nomStockage->getNom() != $espacePlein->getNom()) {
+        } 
+        elseif ($nomStockage->getNom() != $nomStockage->getNom()) { // Sinon (restructurable ou non, mais taille restante déjà suffisante)
             $listStockage->insertValues($nomStockage); 
         }
     }
@@ -31,12 +36,18 @@ function debutRecherche ($stockage, $objetAPlacer){
 
     //Recherche dans tous les espaces de stockage
     for ($nomStockage=0; $nomStockage < $longueurListeStockage-1; $nomStockage++) { 
-        $trouver = false;
-        Recherche($score, $trouver,  $nomDossierTrouver, $dossierParent, $objetAPlacer);
+        $listStockage->next(); // A chaque itération de la boucle for, on passe à l'objet suivant de listStockage
+
+        recherche($score, $trouver,  $nomDossierTrouver, $dossierParent, $objetAPlacer);
         //Recupération du nom de l'espace de stockage comportent la meilleur position
+
         if ($trouver == true) {
-            $nomEspaceStockageTrouver = $stockage[$nomStockage];
+            $nomDossierTrouver = $listStockage->current();
         }
     }
 }
+
+debutRecherche($drive,$fichier);
+
+
 ?>
