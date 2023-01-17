f12b2c906820adc0a9f462da8f179371e8f741f7
Eduardo Da Silva <93222151+EduardoXavier612@users.noreply.github.com>
2023-01-17 13:44:22 +0100

    remise en forme du github

---
+++ src/classe/Stockage.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * @file Stockage.php
+ * @author cabrol (ycabrol@iutbayonne.univ-pau.fr)
+ * @brief fichier contenant la classe Tag
+ * @detais Classe représentent un espace de stockage physique à partir d'un nom, d'une taille, d'une tailleMax, d'un chemin et d'une restructuration
+ * @version 2
+ * @date 2021-03-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/**
+ * Classe représentent un espace de stockage physique à partir d'un nom, d'une taille, d'une tailleMax, d'un chemin et d'une restructuration
+ */
+class Stockage{
+ 
+  // Attributs
+  /**
+   * Représentation du nom que va posséder l'objet
+   *
+   * @var string
+   */
+  public $nom;
+  
+  /**
+   * Représentation de la taille que va posséder l'objet
+   *
+   * @var integer
+   */
+  public $taille;
+  
+  /**
+   * Représentation de la tailleMax que va posséder l'objet
+   *
+   * @var integer
+   */
+  public $tailleMax;
+  
+  /**
+   * Représentation du chemin que va posséder l'objet
+   *
+   * @var string
+   */
+  public $chemin;
+  
+  /**
+   * Représentation de la restructuration que va posséder l'objet
+   *
+   * @var bool
+   */
+  public $restructurable;
+  
+  /**
+   * Représentaion de l'enfant de l'objet
+   *
+   * @var Dossier
+   */
+  public $maRacine;
+  
+  
+  // CONSTRUCTEUR
+  /**
+   * Constructeur de la classe Stockage demandant en paramètre le nom, la taille, la tailleMax, le chemin et si le stockage est restructurable
+   *
+   * @param string $nom           Représentation du nom que va posséder l'objet
+   * @param integer $taille       Représentation de la taille que va posséder l'objet
+   * @param integer $tailleMax    Représentation de la tailleMax que va posséder l'objet
+   * @param string $chemin        Représentation du chemin que va posséder l'objet
+   * @param bool $restructurable  Représentation de la restructuration que va posséder l'objet
+   */
+  public function __construct($nom, $taille, $chemin, $tailleMax ,$restructurable)
+  {
+    $this->restructurable = $restructurable;
+    $this->nom = $nom;
+    $this->taille = $taille;
+    $this->chemin = $chemin;
+    $this->tailleMax = $tailleMax;    
+  }
+  
+  /**
+   * Fonction de clonage de l'objet Stockage
+   *
+   * @param Stockage $Stockage objet à cloner
+   */
+  public function Clone($Stockage)
+  {
+    $this->restructurable = $Stockage->restructurable;
+    $this->nom = $Stockage->nom;
+    $this->taille = $Stockage->taille;
+    $this->chemin = $Stockage->chemin;
+    $this->tailleMax = $Stockage->tailleMax;
+  }
+
+  // DESTRUCTEUR
+  /**
+   * Destructeur de la classe
+   */
+  public function __destuct(){
+   echo 'Destroying: ', $this->restructurable;
+   echo 'Destroying: ', $this->nom;
+   echo 'Destroying: ', $this->taille;
+   echo 'Destroying: ', $this->chemin;
+   echo 'Destroying: ', $this->tailleMax;
+  }
+  
+  // ENCAPSULATION
+  //public
+  /**
+   * Retourne le nom de l'object Stockage
+   *
+   * @return string
+   */
+  public function getNom(){return $this->nom;}
+  
+  /**
+   * Retourne la taille de l'object Stockage
+   *
+   * @return integer
+   */
+  public function getTaille(){return $this->taille;}
+  
+  /**
+   * Retourne la tailleMax de l'object Stockage
+   *
+   * @return integer
+   */
+  public function getTailleMax(){return $this->tailleMax;}
+  
+  /**
+   * Retourne le chemin de l'object Stockage
+   *
+   * @return string
+   */
+  public function getChemin(){return $this->chemin;}
+ 
+  /**
+   * Retourne true si l'object Sctockage est restructuration
+   * false sinon
+   *
+   * @return bool
+   */
+  public function getRestructurable(){return $this->restructurable;}
+  
+  /**
+   * Retourne la racine de l'object Stockage
+   *
+   * @return Dossier
+   */
+  public function getMaRacine(){return $this->maRacine;}
+ 
+  /**
+   * Modifie le nom de l'object Stockage
+   *
+   * @param string $nom Représentation du nom que va posséder l'objet
+   */
+  public function setNom($nom){$this->nom = $nom;}
+  
+  /**
+   * Modifie la taille de l'object Stockage
+   *
+   * @param integer $taille Représentation de la taille que va posséder l'objet
+   */
+  public function setTaille($taille){$this->taille = $taille;}
+  
+  /**
+   * Modifie la tailleMax de l'object Stockage
+   *
+   * @param integer $tailleMax Représentation de la tailleMax que va posséder l'objet
+   */
+  public function setTailleMax($tailleMax){$this->tailleMax = $tailleMax;}
+  
+  /**
+   * Modifie le chemin de l'object Stockage
+   *
+   * @param string $chemin Représentation du chemin que va posséder l'objet
+   */
+  public function setChemin($chemin){$this->chemin = $chemin;}
+  
+  /**
+   * Modifie l'attribue restructurable de l'object Stockage
+   *
+   * @param bool $restructurable Représentation de la restructuration que va posséder l'objet
+   */
+  public function setRestructurable($restructurable){$this->restructurable = $restructurable;}
+  
+  /**
+   * Modifie la racine de l'object Stockage
+   *
+   * @param Dossier $racine Représentaion de l'enfant de l'objet
+   */
+  public function setMaRacine($racine){$this->maRacine = $racine;}
+  
+  // MÉTHODE USUELLES
+
+  // MÉTHODE SPÉCIFIQUE : NON
+}
+?>
---
+++ src/classe/Tag.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @file Tag.php
+ * @author cabrol (ycabrol@iutbayonne.univ-pau.fr)
+ * @brief fichier contenant la classe Tag
+ * @detail Classe représentant un tag que l'on donne a des object pour les spécifier
+ * @version 2
+ * @date 2021-03-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+ 
+/**
+ * Classe représentant un tag que l'on donne a des object pour les spécifier
+ */
+class Tag{ 
+  // ATTRIBUTS
+  /**
+   * Représentation du titre(nom) que possède un tag
+   *
+   * @var string
+   */
+  public $titre;
+  
+  // CONSTRUCTEUR
+  /**
+   * Constructeur de la classe
+   *
+   * @param string $titre Représentation du titre(nom) que possède un tag
+   */
+  public function __construct($titre)
+  {
+    $this->titre = $titre;      
+  }
+  
+  // DESTRUCTEUR
+  /**
+   * Destructeur de la classe
+   */
+  public function __destuct(){
+      echo 'Destroying: ', $this->titre;
+  }
+  
+  // ENCAPSULATION
+  //public
+  /**
+   * Retourne le titre de l'object Tag
+   *
+   * @return string
+   */
+  public function getTitre(){return  $this->titre;}
+  
+  /**
+   * Modifie le titre de l'object Tag
+   *
+   * @param string $titre Représentation du titre(nom) que possède un tag
+   */
+  public function setTitre($titre){$this->titre = $titre;}
+  
+  // MÉTHODE USUELLE : NON
+  
+  // MÉTHODE SPÉCIFIQUE : NON
+}
+?>
