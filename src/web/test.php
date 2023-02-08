23490c23c24f0da9cf26c743dfb22fab2aba3436
yoann cabrol <cabrolyoann@hotmail.fr>
2023-02-08 15:40:37 +0100

    ajouter des image pour les fichier

--- src/web/test.php
+++ src/web/test.php
@@ -6,6 +6,8 @@ echo $header;
 echo '<p> Salut </p>';
 echo '<h2> Test en vrai osef</h2>';
 
+echo '<a href="https://www.flaticon.com/free-icons/interface" title="interface icons">Interface icons created by Freepik - Flaticon</a>';
+
 echo $footer;
 
 ?>
